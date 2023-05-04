71b2973f84cd46777a470613ffa46d669004dbc3
Monaim <monaimer99@gmail.com>
2023-05-04 14:19:02 +0100

    dashboard v1 commit

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', patient_list, name='patient_list'),
+    path('cars_dash/', cars_dash, name='cars_dash'),
+    path('patient/', patient_list, name='patient_list'),
     path('create/', create_patient, name='create_patient'),
     path('update/<int:pk>', update_patient, name='update_patient'),
     path('delete/<int:pk>', delete_patient, name='delete_patient'),
-    path('search/', search, name='search')
+    path('search/', search, name='search'),
 
 ]
